a7bc998891f49cf16666ce54ac29a3776f0133d4
hkhk98708154 <hkhk01312022@gmail.com>
2025-05-21 13:47:54 +0900

    シナリオ調整

--- scenario.js
+++ scenario.js
@@ -7,7 +7,7 @@ const scenarioData = [
     "set char nyanma nyanma-def_closed.webp",
     // セリフ
     "にゃんまちゃん「こんにちは！にゃんまちゃん参上やで！」",
-    "set char nyanluna nyanluna-def_closed.webp",
+    "set char nyanluna nyanluna-mu_closed.webp",
     "にゃんるなちゃん「おはよ…ぼーっとしてたけど、来てくれたんだね。」",
     "set char nyanmi nyanmi-def_closed.webp",
     "にゃんみちゃん「やっほー！みんな元気だねっ♪」",
@@ -27,7 +27,7 @@ const scenarioData = [
     "set char nyanma nyanma-waku_closed.webp",
     "にゃんまちゃん「それはな、にゃんまるコインはただのコインやなくて、みんなの応援で育つDAOトークンやからやで！」",
     "にゃんまちゃん「みんなでワイワイ盛り上がれるし、保有者が増えるほど強くなるんや！」",
-    "set char nyanluna nyanluna-def_closed.webp",
+    "set char nyanluna nyanluna-niko_closed.webp",
     "にゃんるなちゃん「そうだね…みんなで支え合うって、なんかあったかいよね…」",
     "set char nyanmi nyanmi-waku_closed.webp",
     "にゃんみちゃん「しかも、保有者が多いほどにゃんまるコインの価値もどんどん上がるから、みんなやる気満々なんだよっ♪」",
@@ -37,7 +37,7 @@ const scenarioData = [
     // 終わりの挨拶
     "set char nyanma nyanma-niko_closed.webp",
     "にゃんまちゃん「またこうやってみんなで集まって話そうね！」",
-    "set char nyanluna nyanluna-def_closed.webp",
+    "set char nyanluna nyanluna-niko_closed.webp",
     "にゃんるなちゃん「うん…またいつでも赤ねこ亭においでね。」",
     "set char nyanmi nyanmi-niko_closed.webp",
     "にゃんみちゃん「うん、今度はおいしいスイーツも食べたいな！またねっ♪」",
